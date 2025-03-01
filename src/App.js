42ec974659a2d5baa60ec1cd2770e07f3215eff2
uo258538 <uo258538@uniovi.es>
2025-03-01 17:49:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './index.css';
+import Ver from './components/Ver';
+import Actividades from './components/Actividades';
+import Comer from './components/Comer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Ver/>
+      <Actividades/>
+      <Comer/>
+      <Footer />
     </div>
   );
 }
